b94bcf10138bfa41f9165f8de88934ccc0dbac08
fancyChuan <1247375074@qq.com>
2019-01-20 20:31:52 +0800

    springmvc-表单校验完整案例-笔记

--- springmvc/src/main/java/com/fancychuan/springmvc/controller/User2Controller.java
+++ springmvc/src/main/java/com/fancychuan/springmvc/controller/User2Controller.java
@@ -15,9 +15,13 @@ public class User2Controller {
     @RequestMapping(value="/registerform")
     public String registerform(Model model) {
         model.addAttribute(new User2());
-        return "register2";
+        return "register2"; // 这里return的字符串，SpringMVC回到views中寻找相同命名的jsp文件
     }
 
+    /**
+     * @Valid @ModelAttribute("user2") User2 user2 的意思是把数据绑定到@ModelAttribute后进行数据校验
+     * Errors 保存校验信息
+     */
     @RequestMapping(value="/register2")
     public String register(@Valid @ModelAttribute("user2") User2 user2, Errors errors, Model model) {
         if (errors.hasErrors()) {
