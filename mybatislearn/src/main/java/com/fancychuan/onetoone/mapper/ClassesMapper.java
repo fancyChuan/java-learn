7725cd8d8eb552936cc6ad240eaf39fdc98687c1
fancyChuan <1247375074@qq.com>
2019-01-21 23:53:13 +0800

    mybatis一对多

--- mybatislearn/src/main/java/com/fancychuan/onetoone/mapper/ClassesMapper.java
+++ mybatislearn/src/main/java/com/fancychuan/onetoone/mapper/ClassesMapper.java
@@ -5,4 +5,6 @@ import com.fancychuan.onetoone.model.Classes;
 public interface ClassesMapper {
 
     public Classes selectClassById(Integer id) throws Exception;
+
+    public Classes selectClassAndStudentsById(Integer id) throws Exception;
 }
--- mybatislearn/src/main/java/com/fancychuan/onetoone/model/Classes.java
+++ mybatislearn/src/main/java/com/fancychuan/onetoone/model/Classes.java
@@ -1,17 +1,23 @@
 package com.fancychuan.onetoone.model;
 
+import com.fancychuan.onetomany.model.Student;
+
+import java.util.List;
+
 public class Classes {
     private Integer id;
     private String name;
     private HeadTeacher teacher;
+    private List<Student> students;
 
     public Classes() {
     }
 
-    public Classes(Integer id, String name, HeadTeacher teacher) {
+    public Classes(Integer id, String name, HeadTeacher teacher, List<Student> students) {
         this.id = id;
         this.name = name;
         this.teacher = teacher;
+        this.students = students;
     }
 
     public Integer getId() {
@@ -36,5 +42,13 @@ public class Classes {
 
     public void setTeacher(HeadTeacher teacher) {
         this.teacher = teacher;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
     }
 }
