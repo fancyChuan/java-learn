3ea7dc868bd911944936623a7ddf50551db50c58
fancyChuan <1247375074@qq.com>
2019-01-27 14:52:22 +0800

    mybatis注解

--- mybatislearn/src/main/java/Main.java
+++ mybatislearn/src/main/java/Main.java
@@ -12,6 +12,20 @@
  *  【一对多】
  *      * 主要使用collection来做一多的映射关系。见ClassesMapper.xml文件
  *      * 这里使用了 嵌套结果映射 的方式
+ * 20190124
+ *  【多对多】
+ *      * 跟一对多相比，加了个中间的映射类 StudentCourseLink，对于这个类来说，就是一对多。
+ *
+ * 【理解】
+ *      1. mybatis的本质把JDBC返回的每一行数据进行封装
+ *
+ *  1	Tom	male	18	1	1	Math	5
+ *  1	Tom	male	18	1	2	Computer	4
+ *
+ *      比如上面的就是一对多，后三个变量用collection来封装。见 StudentMapper.xml
+ *
+ * 20190126
+ *  【注解】
  */
 public class Main {
 }
