3f9d1ea54804ba189c4465df70b6eb012cbc6a80
fancyChuan <1247375074@qq.com>
2019-03-16 16:36:04 +0800

    注解：实践javac编译

--- javase/src/annotation/Main.java
+++ javase/src/annotation/Main.java
@@ -18,15 +18,16 @@ public class Main {
      * 抑制这种警告的方法：
      *  - 使用@SafeVarargs修饰
      *  - 使用@SuppressWarnings("unchecked")
-     *  - 编译时使用 -Xlint:varargs选项
-     *
+     *  - 编译时使用 -Xlint:varargs 选项 [这个结果经过实践: 是会得到具体的警告信息，而不是抑制警告]
+     *      E:\JavaWorkshop\java-learn\javase\src\annotation>javac -encoding "utf-8" -Xlint:varargs Main.java
      *  下面的方法在 javac 编译的时候会报错
      */
     @SafeVarargs
+    @SuppressWarnings("unchecked") // 加上上面这两行注解就可以让编译器对下面的方法不报警告
     public static void testSafeVarargs(List<String>... listStrArray) {
         // 下面演示第(1)种情况
         List list = new ArrayList<Integer>();
-        list.add(20);
+        list.add(20); // 这个地方会有"unchecked"的警告
         List<String> listStr = list; // 整个地方产生堆污染
         System.out.println(listStr.get(0)); // 这里在运行时报错ClassCastException: java.lang.Integer cannot be cast to java.lang.String
         // 下面演示第(2)种情况
