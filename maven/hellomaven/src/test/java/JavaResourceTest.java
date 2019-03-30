2450912638fa44a7fd28879b0a2c68097667d1d2
fancyChuan <1247375074@qq.com>
2019-03-31 01:49:18 +0800

    Maven：first practice

---
+++ maven/hellomaven/src/test/java/JavaResourceTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class JavaResourceTest {
+
+    @Test
+    public void test() throws IOException {
+        JavaResource resourceLoader = new JavaResource();
+        resourceLoader.loadProperties1();
+    }
+}
