50d27b2c4795c646612a1198532f114f249f6f17
fancyChuan <1247375074@qq.com>
2019-07-10 00:06:51 +0800

    SpringBoot：yml-配置文件与bean绑定

--- springboot/src/main/java/cn/fancychuan/springboot/bean/Person.java
+++ springboot/src/main/java/cn/fancychuan/springboot/bean/Person.java
@@ -1,9 +1,22 @@
 package cn.fancychuan.springboot.bean;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 将 application.yml中配置的属性的值映射到组件中
+ *
+ * @ConfigurationProperties 告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定
+ *  prefix = "person" 表示将配置文件中的person下面的所有属性进行一一映射
+ *
+ * @Component 只有这个组件是容器中的组件，才能使用容器提供的@ConfigurationProperties功能
+ */
+@Component
+@ConfigurationProperties(prefix = "person")
 public class Person {
 
     private String lastName;
