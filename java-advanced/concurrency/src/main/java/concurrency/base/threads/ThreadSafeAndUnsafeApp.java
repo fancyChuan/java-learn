6616773baea1aa8fdd9e8da77191a786b4b08970
fancyChuan <1247375074@qq.com>
2019-08-13 23:16:11 +0800

    高级java：并发编程-java并发机制的底层原理-原子性操作

--- java-advanced/concurrency/src/main/java/concurrency/base/threads/ThreadSafeAndUnsafeApp.java
+++ java-advanced/concurrency/src/main/java/concurrency/base/threads/ThreadSafeAndUnsafeApp.java
@@ -47,3 +47,31 @@ class ThreadSafeIntegerVolatile {
         this.value = value;
     }
 }
+
+//***********************************************************************//
+
+/**
+ * 设计计数字器的时候，就需要考虑线程安全的问题
+ */
+class ThreadUnSafeCount {
+    private long value;
+    public void inc() {
+        ++ value; // 先加1再使用
+    }
+    public long getValue() {
+        return value;
+    }
+}
+
+/**
+ * synchronized可以解决线程安全问题，但是因为是独占锁，大大降低了并发性
+ */
+class ThreadSafeCount {
+    private long value;
+    public synchronized void inc() {
+        ++ value;
+    }
+    public synchronized long getValue() { // 虽然是读操作，但是这里的synchronized不能去掉，因为我们需要靠它来保证value的内存可见性
+        return value;
+    }
+}
