f8ce8e51b4e94e6e43986575b3c50198b015019f
fancyChuan <1247375074@qq.com>
2019-08-22 23:45:53 +0800

    高级java：并发编程-线程基础-java指令重排序-实例注释

--- java-advanced/concurrency/src/main/java/concurrency/base/threads/CommandReSortApp.java
+++ java-advanced/concurrency/src/main/java/concurrency/base/threads/CommandReSortApp.java
@@ -2,7 +2,22 @@ package concurrency.base.threads;
 
 /**
  * java指令重排序下的并发编程问题
-*/
+ *
+ * 这个示例会出现多种情况。
+ * 1. 正常的输出4
+ * 2. 输出0
+ * 3. 没有输出。如下所示
+[x]main exit
+Read Thread...
+Write Thread set over...
+ *
+ * 原因分析：这个代码的变量没有声明为volatile（volatile本身可以避免指令重排序问题），也没有任何同步措施，会有变量内存可见性问题。另外，也存在指令重排序的问题
+ *
+ * 代码(3)(4)不存在依赖关系，所以可能WriteThread中先执行了(4)再执行的(3).
+ * 而执行了(4)以后可能ReadThread已经执行完了(1)，并且在(3)执行之前开始执行(2)这时就会输出0
+ *
+ * 结论：重排序在多线程下会导致不可预期的运行结果，我们使用volatile来修饰ready变量就可以避免重排序和内存可见性问题
+ */
 public class CommandReSortApp {
     private static int num = 0;
     private static boolean ready = false;
@@ -14,7 +29,7 @@ public class CommandReSortApp {
         write.start();
         Thread.sleep(10);
         read.interrupt();
-        System.out.println("[x]main exit");
+        System.out.println("[x]main exit: " + num + "    " + ready);
     }
     // 读线程
     public static class ReadThread extends Thread {
