d1ebe5693649c89bbafdc1a6d5935a046fb3a887
fancyChuan <1247375074@qq.com>
2019-09-08 12:55:09 +0800

    高级java：并发编程-基础篇-死锁-应用

--- java-advanced/concurrency/src/main/java/concurrency/base/threads/DeadLockApp.java
+++ java-advanced/concurrency/src/main/java/concurrency/base/threads/DeadLockApp.java
@@ -9,20 +9,22 @@ public class DeadLockApp {
     private static String B = "B";
 
     public static void main(String[] args) {
-
+        deadLock();
     }
 
     private static void deadLock() {
         Thread t1 = new Thread(new Runnable() {
             public void run() {
                 synchronized (A) {
+                    System.out.println("[线程1]拿到资源A");
                     try {
                         Thread.currentThread().sleep(2000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+                    System.out.println("[线程1]wait for B ...");
                     synchronized (B) {
-                        System.out.println("111");
+                        System.out.println("[线程1]拿到资源B");
                     }
                 }
             }
@@ -31,8 +33,10 @@ public class DeadLockApp {
         Thread t2 = new Thread(new Runnable() {
             public void run() {
                 synchronized (B) {
+                    System.out.println("[线程2]拿到资源B");
+                    System.out.println("[线程2]wait for A ...");
                     synchronized (A) {
-                        System.out.println("222");
+                        System.out.println("[线程2]拿到资源A啦");
                     }
                 }
             }
