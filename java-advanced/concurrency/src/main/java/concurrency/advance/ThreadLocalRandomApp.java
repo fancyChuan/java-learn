e7ba0591f8e16a76580e60a88c1eef58bd79b53d
fancyChuan <1247375074@qq.com>
2019-09-15 18:29:04 +0800

    高级java：并发编程-高级篇-ThreadLocalRandom的使用

---
+++ java-advanced/concurrency/src/main/java/concurrency/advance/ThreadLocalRandomApp.java
@@ -0,0 +1,19 @@
+package concurrency.advance;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * ThreadLocalRandom的使用
+ */
+public class ThreadLocalRandomApp {
+    public static void main(String[] args) {
+        demo();
+    }
+
+    public static void demo() {
+        ThreadLocalRandom random = ThreadLocalRandom.current(); // 获得当前线程的随机生成器
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(random.nextInt(5));
+        }
+    }
+}
