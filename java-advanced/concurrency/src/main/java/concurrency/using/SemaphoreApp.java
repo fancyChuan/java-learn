ade1498535068700e65c6e6ee1b18790e0680bf9
fancyChuan <1247375074@qq.com>
2019-09-21 23:55:40 +0800

    高级java：并发编程-并发包-Semaphore的使用-设置不允许中断

--- java-advanced/concurrency/src/main/java/concurrency/using/SemaphoreApp.java
+++ java-advanced/concurrency/src/main/java/concurrency/using/SemaphoreApp.java
@@ -51,27 +51,42 @@ public class SemaphoreApp {
     /**
      *
      */
-    public void testInterrupt() {
-        Service service = new Service(60);
-        Thread subThread = new Thread(new MyRunable(service), "subThread");
-        subThread.start();
-        subThread.interrupt();
-        System.out.println("main thread 中断了 " + subThread.getName());
+    public void testInterrupt() throws InterruptedException {
+        Service service = new Service(10);
+        Thread thread1 = new Thread(new MyRunable(service), "thread1");
+        thread1.start();
+        Thread.sleep(2000);
+        thread1.interrupt();
+        System.out.println("main thread 中断了 " + thread1.getName());
+
+        Thread.sleep(5000);
+        System.out.println("====================");
+        Service service1 = new Service(11);
+        service1.setInterruptFlag(false); // 设置不允许中断
+        Thread thread2 = new Thread(new MyRunable(service1), "thread2");
+        thread2.start();
+        Thread.sleep(1000);
+        thread2.interrupt();
     }
 
     class Service {
         int sleepTime;
-        public Service(int sleepTime) {
+        boolean interruptFlag = true;
+        public Service(int sleepTime) { // 休眠多久
             this.sleepTime = sleepTime * 1000;
         }
         public Service() {
             this.sleepTime = 3000;
         }
 
-        private Semaphore semaphore = new Semaphore(2); // 构造参数permits是许可的意思，表示同一时间内最多允许多少个线程同时执行acquire()/release()
+        private Semaphore semaphore = new Semaphore(1); // 构造参数permits是许可的意思，表示同一时间内最多允许多少个线程同时执行acquire()/release()
         public void testMethod() {
             try {
-                semaphore.acquire(); // 也可以加一个参数，表示一次消费多少个许可
+                if (interruptFlag) {
+                    semaphore.acquire(); // 也可以加一个参数，表示一次消费多少个许可
+                } else {
+                    semaphore.acquireUninterruptibly(); // 不能被中断
+                }
                 System.out.println(Thread.currentThread().getName() + " begin time = " + System.currentTimeMillis());
                 Thread.sleep(sleepTime);
                 System.out.println(Thread.currentThread().getName() + " end   time = " + System.currentTimeMillis());
@@ -80,6 +95,9 @@ public class SemaphoreApp {
                 System.out.println(Thread.currentThread().getName() + " was interrupted!");
                 e.printStackTrace();
             }
+        }
+        public void setInterruptFlag(boolean interruptFlag) {
+            this.interruptFlag = interruptFlag;
         }
     }
 
