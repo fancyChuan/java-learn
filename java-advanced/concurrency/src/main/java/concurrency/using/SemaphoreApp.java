d78867d50c5b7e3b86e2a77550ab6ccba7c4c6ab
fancyChuan <1247375074@qq.com>
2019-09-21 23:22:18 +0800

    高级java：并发编程-并发包-Semaphore的使用-动态添加许可

--- java-advanced/concurrency/src/main/java/concurrency/using/SemaphoreApp.java
+++ java-advanced/concurrency/src/main/java/concurrency/using/SemaphoreApp.java
@@ -6,14 +6,19 @@ import java.util.concurrent.Semaphore;
  * Semaphore的使用
  *  1. new Semaphore(n) 表示同一时间只有n个线程同时执行acquire方法。 n>1的时候不能保证线程安全性
  *  2. semaphore.acquire(2) 表示使用2个许可
+ *  3. 使用release方法动态添加许可
  */
 public class SemaphoreApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SemaphoreApp app = new SemaphoreApp();
-        app.test1();
+        // app.testBaseUsing();
+        app.testAddPermits();
     }
 
-    public void test1() {
+    /**
+     * 基本用法
+     */
+    public void testBaseUsing() {
         class MyRunable implements Runnable {
             private Service service;
             public MyRunable(Service service) {
@@ -27,8 +32,8 @@ public class SemaphoreApp {
         Service service = new Service();
         Thread threadA = new Thread(new MyRunable(service));
         threadA.setName("A");
-        Thread threadB = new Thread(new MyRunable(service));
-        threadB.setName("B");
+        Thread threadB = new Thread(new MyRunable(service), "B"); // 这种方式也可以设置线程名
+        // threadB.setName("B");
         Thread threadC = new Thread(new MyRunable(service));
         threadC.setName("C");
         threadA.start();
@@ -36,8 +41,24 @@ public class SemaphoreApp {
         threadC.start();
     }
 
+    /**
+     * 动态增加permits
+     */
+    public void testAddPermits() throws InterruptedException {
+        Semaphore semaphore = new Semaphore(2);
+        semaphore.acquire();
+        semaphore.acquire();
+        System.out.println("可用permits: " + semaphore.availablePermits());
+        semaphore.release();
+        semaphore.release();
+        semaphore.release(); // 多释放了一个，所以可用就变成了3
+        System.out.println("可用permits: " + semaphore.availablePermits());
+        semaphore.release(3); // 再释放了3个，可用就变为了6个
+        System.out.println("可用permits: " + semaphore.availablePermits());
+    }
+
     class Service {
-        private Semaphore semaphore = new Semaphore(1); // 构造参数permits是许可的意思，表示同一时间内最多允许多少个线程同时执行acquire()/release()
+        private Semaphore semaphore = new Semaphore(2); // 构造参数permits是许可的意思，表示同一时间内最多允许多少个线程同时执行acquire()/release()
         public void testMethod() {
             try {
                 semaphore.acquire(); // 也可以加一个参数，表示一次消费多少个许可
