210c870d8d80053ab6591378fff91b55f500b569
touna_fancy <1247375074@qq.com>
2019-10-10 20:16:20 +0800

    javase：多线程-线程通信-使用传统的wait/notify

---
+++ javase/src/multithreads/ThreadWaitNotifyApp.java
@@ -0,0 +1,74 @@
+package multithreads;
+
+import java.util.Random;
+
+/**
+ * 传统的线程通信：模拟账户只允许一次存款一次取款交替进行
+ *
+ */
+public class ThreadWaitNotifyApp {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadWaitNotifyApp app = new ThreadWaitNotifyApp();
+
+        Account account = app.new Account(0);
+        Random random = new Random();
+        Runnable depositRunnable = () -> {
+            for (int i = 0; i < 10; i++) {
+                account.deposit(random.nextInt(10), i);
+            }
+        };
+        Runnable drawRunnable = () -> {
+            for (int i = 0; i < 15; i++) {
+                account.draw(random.nextInt(10), i);
+            }
+        };
+        // 三个人存款，每个人存10次。2个人取款，每个人
+        new Thread(depositRunnable, "人物A").start();
+        new Thread(depositRunnable, "人物B").start();
+        new Thread(depositRunnable, "人物C").start();
+        new Thread(drawRunnable, "AAAA").start();
+        new Thread(drawRunnable, "YYYY").start();
+    }
+
+    class Account {
+        private int balance;
+        private boolean drawFlag; // flag表示是否允许取款
+        public Account(int balance) {
+            this.balance = balance;
+            this.drawFlag = false; // 要求实现存款
+        }
+        // 余额不能随便修改，只提供getter方法
+        public double getBalance() {
+            return balance;
+        }
+        public synchronized void draw(int drawAmt, int index) {
+            try {
+                if (!drawFlag) { // 不允许取款
+                    wait();
+                } else {
+                    balance -= drawAmt;
+                    System.out.println(Thread.currentThread().getName() + " " + index + "次开始取款 " + drawAmt + "\t之后余额为 " + balance);
+                    drawFlag = false;
+                    notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        public synchronized void deposit(int depositAmt, int index) {
+            try {
+                if (drawFlag) {
+                    wait();
+                } else {
+                    balance += depositAmt;
+                    System.out.println(Thread.currentThread().getName() + " " + index + "次开始存款 " + depositAmt + "\t之后余额为 " + balance);
+                    drawFlag = true;
+                    notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
