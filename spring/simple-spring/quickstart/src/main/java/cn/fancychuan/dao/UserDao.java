227fcb12559523e0116f66538266dfd2a07239e0
fancyChuan <1247375074@qq.com>
2019-10-20 10:45:56 +0800

    Spring：目录重构2

---
+++ spring/simple-spring/quickstart/src/main/java/cn/fancychuan/dao/UserDao.java
@@ -0,0 +1,39 @@
+package cn.fancychuan.dao;
+
+import cn.fancychuan.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository // 定义为一个DAO
+public class UserDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public int getMatchCount(String userName, String password) {
+        String sqlStr = "select count(*) from t_user where user_name=? and password=?";
+        int result = jdbcTemplate.queryForObject(sqlStr, new Object[]{userName, password}, Integer.class);
+        return result;
+    }
+
+    public User findUserByUserName(final String userName) {
+        final User user = new User();
+        String sqlStr = "select user_id, user_name from t_user where user_name=?";
+        jdbcTemplate.query(sqlStr, new Object[]{userName}, new RowCallbackHandler() {
+            public void processRow(ResultSet resultSet) throws SQLException {
+                user.setUserId(resultSet.getInt("user_id"));
+                user.setUserName(userName);
+            }
+        });
+        return user;
+    }
+
+    public void updateLoginInfo(User user) {
+        String sqlStr = "update t_user set last_visit=?, last_ip=? where user_id=?";
+        jdbcTemplate.update(sqlStr, new Object[]{user.getLastVisit(), user.getLastIp(), user.getUserId()});
+    }
+}
